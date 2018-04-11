38fc60e38ac28b85ac1d7c669645b66f09da72f4
Daniton2906 <ddiomedi2906@gmail.com>
2018-04-11 08:07:40 -0300

    Minor change

--- T1/main.py
+++ T1/main.py
@@ -5,13 +5,12 @@ from src.third_phase import Detector
 
 # Global variables
 FRAMES_PER_CELL = 10
-K = 10
-# s = (320, 180); s2 = (432, 768)
-RESIZE = (320, 180)  #(160, 90)
+K = 15
+RESIZE = (320, 180)  #(160, 90), (432, 768)
 FPS_RATE = 30
-TOLERANCE = 90
-EPSILON = 3
-DESCRIP_MANAGER = IntensityVectorDescriptor(20, 11) # HistDescriptor(16, 4, 4, RESIZE)
+TOLERANCE = 70
+EPSILON = 5
+DESCRIP_MANAGER = IntensityVectorDescriptor(20, 11)  # HistDescriptor(16, 4, 4, RESIZE)
 
 # Counter for time
 global_counter = 0
@@ -27,6 +26,7 @@ else:
 if seq[0] == '1':
     print("Codificando comerciales...")
     start = time.time()
+
     comerciales_list = load_filenames(C_FOLDER, "mpg")
     print("Comerciales encontrados: {}".format(len(comerciales_list)))
 
@@ -41,33 +41,33 @@ if seq[0] == '1':
 if seq[1] == '1':
     print("Codificando television...")
     start = time.time()
+
     tele_list = load_filenames(TV_FOLDER, "mp4")
     print("Videos de televison encontrados: {}".format(tele_list))
 
-    extractor2 = Extractor(tele_list[:1], FRAMES_PER_CELL)
+    extractor2 = Extractor(tele_list, FRAMES_PER_CELL)
     r = extractor2.process_data(MAIN_DATA_FOLDER + TV_FOLDER, DESCRIP_MANAGER)  # max_frames=1500, rsize=RESIZE)
+    extractor2.codify(MAIN_FOLDER + DATA_FOLDER + TV_DESCRIP_FOLDER)
+
     end = time.time(); global_counter += int(end - start)
     print("Videos codificados en {}s...".format(int(end - start)))
-
-    extractor2.codify(MAIN_FOLDER + DATA_FOLDER + TV_DESCRIP_FOLDER)
 
 # Calculate k-nearest frames for each television's frame
 if seq[2] == '1':
     print("Calculando KNFs...")
-    comerc_descriptor_list = load_filenames(C_DESCRIP_FOLDER, "txt")
-    #print(comerc_descriptor_list)
+    start = time.time()
 
-    tele_list = load_filenames(TV_DESCRIP_FOLDER, "txt")[:1]
-    #print(tele_list)
+    comerc_descriptor_list = load_filenames(C_DESCRIP_FOLDER, "txt")
+    tele_list = load_filenames(TV_DESCRIP_FOLDER, "txt")
 
     comparator1 = Comparator(comerc_descriptor_list)
-    start = time.time()
     r = comparator1.load_data(MAIN_DATA_FOLDER + C_DESCRIP_FOLDER)
+
     for tele in tele_list:
         print("calculando KNFs (k = {}) para {}...".format(K, tele))
         result = comparator1.k_nearest_frames(K, DESCRIP_MANAGER, tele, DATA_FOLDER + TV_DESCRIP_FOLDER)
-        #print(result.shape)
         comparator1.write_on_memory(result, tele, DATA_FOLDER + KNF_FOLDER)
+
     end = time.time(); global_counter += int(end - start)
     print("{} frames más cercanos calculados en {}s...".format(K, int(end - start)))
 
@@ -75,14 +75,16 @@ if seq[2] == '1':
 if seq[3] == '1':
     print("Detectando colisiones...")
     start = time.time()
+
     knf_list = load_filenames(KNF_FOLDER, "txt")
     comerc_descriptor_list = load_filenames(C_DESCRIP_FOLDER, "txt")
-    #print(len(knf_list))
+
     detector = Detector(knf_list, K, comerc_descriptor_list)
     detector.load_data(MAIN_DATA_FOLDER + KNF_FOLDER)
     matches = detector.detect_match(EPSILON, TOLERANCE)
 
     detector.filter_write_results("results.txt", MAIN_DATA_FOLDER + RESULTS_FOLDER, EPSILON, FPS_RATE//FRAMES_PER_CELL)
+
     end = time.time(); global_counter += int(end - start)
     print("Detecciones identificadas en {}s...".format(int(end - start)))
 
